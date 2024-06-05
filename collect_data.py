73f9d1a4f88b666d0194e036f960c9f05428bbd1
Nuno Fonseca Florencio <nunofonsecaflorencio@gmail.com>
2024-06-05 10:25:00 +0200

    Initial Commit

---
+++ collect_data.py
@@ -0,0 +1,74 @@
+import uuid
+import os
+import cv2
+import numpy as np
+import time
+import mediapipe as mp
+from preprocessing import *
+
+
+dataset_path = os.path.join('dataset')
+if not os.path.exists(dataset_path):
+    os.makedirs(dataset_path)
+    
+actions = [name for name in os.listdir(dataset_path) if os.path.isdir(os.path.join(dataset_path, name))]
+print("Select an action from the list or create a new one:")
+for i, action in enumerate(actions, 1):
+    print(f"{i}. {action}")
+print(f"{len(actions) + 1}. Create a new action")
+
+choice = int(input("Enter the number of your choice: "))
+if choice == len(actions) + 1:
+    action = input("Enter the name of the new action: ")
+    action_path = os.path.join(dataset_path, action)
+    os.makedirs(action_path)
+else:
+    action_path = os.path.join(dataset_path, actions[choice - 1])
+    
+
+with HandLandmarker.create_from_options(options) as landmarker:
+    # Start a webcam capture session:
+    cap = cv2.VideoCapture(0)
+    timestamp = frame_count =  0
+    start_time = time.time()
+    TIMER = 5 # seconds
+    
+    while True:
+        timestamp += 1
+        # Capture a frame from the webcam:
+        ret, frame = cap.read()
+        if not ret:
+            break
+        
+        current_time = time.time()
+        dt = current_time - start_time
+        
+        # Annotate the image with the timer
+        cv2.putText(frame, f"Recording: {action.upper()}", (10, 30), 
+                    cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
+        
+        cv2.putText(frame, f"Captured: {frame_count}", (10, 70), 
+                    cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
+        
+        cv2.putText(frame, f"Countdown: {int(TIMER - dt + 1)}s", (10, 110), 
+                    cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2, cv2.LINE_AA)
+        
+        data, frame = process_frame(frame, landmarker, timestamp) 
+        if dt >= TIMER:
+            start_time = current_time
+            frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+            if data.max() > 0:
+                frame_count += 1
+                np.save(os.path.join(dataset_path, action, str(uuid.uuid4())), data)
+
+
+        # Display the frame with hand landmarks:
+        cv2.imshow('Hand Landmarker', frame)
+
+        # Press 'q' to quit:
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    # Release the webcam capture:
+    cap.release()
+    cv2.destroyAllWindows()
---
+++ preprocessing.py
@@ -0,0 +1,87 @@
+import mediapipe as mp
+from mediapipe import solutions
+from mediapipe.framework.formats import landmark_pb2
+import os
+import numpy as np
+
+hand_model_path = os.path.join('handmodel', 'hand_landmarker.task')
+
+BaseOptions = mp.tasks.BaseOptions
+HandLandmarker = mp.tasks.vision.HandLandmarker
+HandLandmarkerOptions = mp.tasks.vision.HandLandmarkerOptions
+VisionRunningMode = mp.tasks.vision.RunningMode
+
+# Create a hand landmarker instance with the video mode:
+options = HandLandmarkerOptions(
+    base_options=BaseOptions(model_asset_path=hand_model_path),
+    num_hands=2,
+    running_mode=VisionRunningMode.VIDEO)
+
+MARGIN = 10  # pixels
+FONT_SIZE = 1
+FONT_THICKNESS = 1
+HANDEDNESS_TEXT_COLOR = (88, 205, 54) # vibrant green
+
+
+def process_frame(frame, landmarker, timestamp):
+    mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame)
+    hand_landmarker_result = landmarker.detect_for_video(mp_image, timestamp)
+    annotated_image = draw_landmarks_on_image(mp_image.numpy_view(), hand_landmarker_result)
+    data = process_hand_landmarks(hand_landmarker_result)
+    return data, annotated_image
+
+def draw_landmarks_on_image(rgb_image, detection_result):
+  hand_landmarks_list = detection_result.hand_landmarks
+  handedness_list = detection_result.handedness
+  annotated_image = np.copy(rgb_image)
+
+  # Loop through the detected hands to visualize.
+  for idx in range(len(hand_landmarks_list)):
+    hand_landmarks = hand_landmarks_list[idx]
+    handedness = handedness_list[idx]
+
+    # Draw the hand landmarks.
+    hand_landmarks_proto = landmark_pb2.NormalizedLandmarkList()
+    hand_landmarks_proto.landmark.extend([
+      landmark_pb2.NormalizedLandmark(x=landmark.x, y=landmark.y, z=landmark.z) for landmark in hand_landmarks
+    ])
+    solutions.drawing_utils.draw_landmarks(
+      annotated_image,
+      hand_landmarks_proto,
+      solutions.hands.HAND_CONNECTIONS,
+      solutions.drawing_styles.get_default_hand_landmarks_style(),
+      solutions.drawing_styles.get_default_hand_connections_style())
+
+  return annotated_image
+
+
+def process_hand_landmarks(hand_landmarker_result):
+    num_landmarks = 21
+    num_coordinates = 3
+
+    # Initialize empty arrays with zeros for padding
+    left_hand_array = np.zeros((num_landmarks, num_coordinates))
+    right_hand_array = np.zeros((num_landmarks, num_coordinates))
+
+    # Identify hands and extract landmarks
+    for i, category in enumerate(hand_landmarker_result.handedness):
+        hand_type = category[0].category_name
+        landmarks = hand_landmarker_result.hand_landmarks[i]
+        landmarks_array = np.array([[landmark.x, landmark.y, landmark.z] for landmark in landmarks])
+
+        if hand_type == 'Left':
+            left_hand_array = landmarks_array
+        elif hand_type == 'Right':
+            right_hand_array = landmarks_array
+
+    # Combine and flatten the arrays
+    array = np.concatenate([left_hand_array, right_hand_array]).flatten()
+    min_val = np.min(array)
+    max_val = np.max(array)
+    
+    # Check for division by zero
+    if max_val - min_val == 0:
+        return np.zeros_like(array)
+    
+    normalized_array = (array - min_val) / (max_val - min_val)
+    return normalized_array
